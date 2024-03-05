4ebfb2a8b38597392ca2fcee92e889117b280c81
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-03-05 15:51:34 -0500

    mardown preview done

--- src/App.js
+++ src/App.js
@@ -1,48 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import * as marked from 'marked';
 
-marked.setOptions({
-  sanitize: false,
-});
+import React from 'react';
+import { marked } from 'marked';
 
-const App = () => {
-  const [markdown, setMarkdown] = useState(`# Heading 1
-## Heading 2
-[Link to Google](https://www.google.com/)
-\`Inline Code\`
-
-\`\`\`
-// Code block
-function greet() {
-  return 'Hello!';
-}
-\`\`\`
-
-- List item 1
-- List item 2
-
-> Blockquote
-
-![Image](https://via.placeholder.com/150)
-**Bolded Text**`);
-
-  useEffect(() => {
-    marked(markdown);
-  }, [markdown]);
-
-  const handleChange = (e) => {
-    setMarkdown(e.target.value);
-  };
-
+ 
+function App() {
+  const [input, setInput] = React.useState("# H1\n" + "## H2\n" + "\`printf('goodbye world!')\`\n"+ "\`\`\` \n printf('goodbye world!') \n \`\`\` \n"+ "[title](https://www.example.com) \n" + "1. First item 2. Second item 3. Third item \n "+" 'span' \n " + " 	> blockquote  \n"+"![alt text](https://www.w3schools.com/images/lamp.jpg)  "+ " **bold text**  ");
+  let handleInput=(event)=>{
+    setInput(event.target.value)
+  }  
+  let getMarkdownText=()=> {
+    var rawMarkup = marked(input);
+    console.log({rawMarkup})
+    return { __html: rawMarkup };
+  }
+  marked.setOptions({
+    sanitize: false,
+    breaks: true,
+  });
+  
   return (
     <>
-    <div>
-      <h1>markdown</h1>
-      <textarea id="editor" value={markdown} onChange={handleChange}></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdown)}}></div>
-    </div>
-    </>
-  );
-};
+     <h1 className='title'>Markdown preview</h1>
+    <div className="main-container">
+     
+        <div className="editor-container">
+          <div className='editor-bar'><spam>Editor</spam></div>
+          <textarea name="" id="editor" cols="30" rows="10" onChange={handleInput}>{input}</textarea>
+        </div>
 
-export default App;
+        <div className="render-container">
+          <div id="preview" dangerouslySetInnerHTML={getMarkdownText()}></div>
+        </div>
+     
+    </div>
+      </>
+  );
+}
+
+export default App; 
